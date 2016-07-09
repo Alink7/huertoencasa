e5093d13e5c43dcfa1a1b8dca74e85d00a570cb6
xH0wlx <baton_hp@hotmail.com>
2016-07-08 22:12:27 -0400

    Lista de plantas

--- app/src/main/java/com/example/alink/huerto/AgregarACultivoActivity.java
+++ app/src/main/java/com/example/alink/huerto/AgregarACultivoActivity.java
@@ -1,11 +1,21 @@
 package com.example.alink.huerto;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,28 +24,105 @@ import bd.BdSqliteHelper;
 
 public class AgregarACultivoActivity extends AppCompatActivity {
     private List<Cultivo> cultivos;
+    private RecyclerView mRecyclerView;
+    private RecyclerViewAdapterCultivos mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private Spinner ncultivo;
+    Planta planta;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_acultivo);
 
-        // Inicializar
-        cultivos = new ArrayList<>();
-
-        traerCultivos();
-
+        Bundle b = getIntent().getExtras();
+        planta = (Planta)b.get("planta");
         TextView tAgregarCultivo = (TextView)findViewById(R.id.tAgregarCultivo);
 
-        //Si no hay cultivos se muestra la instruccion mas el boton
+        // Inicializar
+        cultivos = new ArrayList<>();
+        traerCultivos();
         if(cultivos.size()==0){
             tAgregarCultivo.setText("No has creado ningún cultivo");
         }else {
-            //si hay cultivos se muestra la lista
-            tAgregarCultivo.setText(cultivos.get(0).getTipoSuelo());
-        }
+            ArrayList<String> nombreArray = new ArrayList<String>();
+
+            for(int i = 0; i< cultivos.size(); i++){
+                nombreArray.add("Cultivo "+(i+1));
+            }
+            ArrayAdapter adapter = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_spinner_dropdown_item, nombreArray);
+
+            ncultivo = (Spinner) findViewById(R.id.cultivo_tipo_suelo);
+
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            ncultivo.setAdapter(adapter);
+
+            Button botonAsignacion = (Button)findViewById(R.id.botonAsignacion);
+
+            botonAsignacion.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    asignarACultivo(v);
+                }
+            });
+
+            //si hay cultivos se muestra la lista     cultivos.get(0).getTipoSuelo()
+
+            /*mAdapter = new RecyclerViewAdapterCultivos(cultivos);
+            mAdapter.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(AgregarACultivoActivity.this, "Ha pulsado el huerto " + mRecyclerView.getChildPosition(v), Toast.LENGTH_SHORT).show();
+
+                    new AlertDialog.Builder(AgregarACultivoActivity.this)
+                            .setTitle( "Confirmación" )
+                            .setMessage( "¿Está seguro que quiere agregarla en el cultivo n°?")
+                            .setPositiveButton( "Sí", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Log.d( "AlertDialog", "Positive" );
+                                }
+                            })
+                            .setNegativeButton( "No", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Log.d( "AlertDialog", "Negative" );
+                                }
+                            } )
+                            .show();
+
+                }
+            });*/
+        }//FIN ELSE
+
+
+    }//FIN ONCREATE
+
+    public void asignarACultivo(View v){
+        BdSqliteHelper bdplantas = new BdSqliteHelper(this, "DBPLANTAS", null, 1);
+        SQLiteDatabase bd = bdplantas.getWritableDatabase();
+
+        //Float largo = Float.parseFloat(txt_cultivo_largo.getText().toString());
+        //Float ancho = Float.parseFloat(txt_cultivo_ancho.getText().toString());
+        //Float profundidad = Float.parseFloat(txt_cultivo_profundidad.getText().toString());
+        String nCultivo = ncultivo.getSelectedItem().toString();
+
+        bd.execSQL("INSERT INTO CultivoContienePlantas (idCultivo , idPlanta) VALUES("+nCultivo+","+planta.getIdPlanta()+")");
+
+        //Mensaje para el usuario
+        Toast.makeText(AgregarACultivoActivity.this, "Planta asignada exitosamente", Toast.LENGTH_SHORT).show();
+
+        //se redirecciona a la pantalla principal
+       // Intent i = new Intent(this, ListaDePlantasActivity.class);
+
+        //cierra todas las actividades (asi al volver atras no vuelve a una pantalla que no deberia ver)
+        //i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        //preparacion para iniciar la nueva activity
+        //i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        //this.startActivity(i);
 
     }
-
 
     private void traerCultivos(){
         BdSqliteHelper bdplantas = new BdSqliteHelper(this, "DBPLANTAS", null, 1);
--- app/src/main/java/com/example/alink/huerto/DetallePlantaActivity.java
+++ app/src/main/java/com/example/alink/huerto/DetallePlantaActivity.java
@@ -22,6 +22,7 @@ import cz.msebera.android.httpclient.Header;
 
 public class DetallePlantaActivity extends AppCompatActivity {
     TextView texto;
+    Planta planta;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,7 +41,12 @@ public class DetallePlantaActivity extends AppCompatActivity {
 
 
         Bundle b = getIntent().getExtras();
-        Planta planta = (Planta)b.get("planta");
+        planta = (Planta)b.get("planta");
+
+
+        tid.setText(planta.getNombre());
+        tnom.setText(planta.getNombreCientifico());
+        tnomC.setText(planta.getClase());
 
         if(planta != null){
             System.out.println(planta.getNombre());
@@ -51,70 +57,16 @@ public class DetallePlantaActivity extends AppCompatActivity {
         }
 
 
-
-
-        RequestParams obj = new RequestParams();
-        obj.put("c", "Planta");
-        obj.put("a", "getplanta");
-        obj.put("id", id+"");
-
-        // Inicializar Animes
-        tarea(obj);
-
         Button agregarACultivo = (Button)findViewById(R.id.botonAgregar);
 
         agregarACultivo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(DetallePlantaActivity.this, AgregarACultivoActivity.class);
+                intent.putExtra("planta", planta);
                 startActivity(intent);
             }
         });
     }
 
-    public void tarea(RequestParams params){
-        AsyncHttpClient client = new AsyncHttpClient();
-        client.get("http://colvin.chillan.ubiobio.cl:8070/nionate/webservice/?", params,new JsonHttpResponseHandler(){
-
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                super.onSuccess(statusCode, headers, response);
-                if(statusCode == 200){
-                    try {
-                        JSONArray arreglo = response.getJSONArray("plantas");
-                        JSONObject planta;
-
-                            Planta plantita = new Planta();
-
-                            JSONObject aux = arreglo.getJSONObject(0);
-                            planta = aux.getJSONObject("planta");
-
-                            plantita.setIdPlanta(planta.getString("idPlanta"));
-                            plantita.setNombre(planta.getString("nombre"));
-                            plantita.setNombreCientifico(planta.getString("nomCientifico"));
-                            plantita.setClase(planta.getString("clase"));
-                            plantita.setDistanciaPlantas(planta.getDouble("distanciaPlantas[cm]"));
-                            plantita.setDistanciaOtrasPlantas(planta.getDouble("distanciaOtrasPlantas[cm]"));
-                            plantita.setProfundidadNecesaria(planta.getDouble("profundidadNecesaria[cm]"));
-                            plantita.setVolumenNecesario(planta.getDouble("volumenNecesario[l]"));
-                            plantita.setnAbono(planta.getString("nAbono"));
-                            plantita.setnRiego(planta.getString("nRiego"));
-                            plantita.setnSol(planta.getString("nSol"));
-                            plantita.setTipoSuelo(planta.getString("tipoSuelo"));
-                            plantita.setnTemperatura(planta.getString("nTemperatura"));
-
-
-                    }catch(JSONException e){
-
-                    }
-                }
-            }
-
-            @Override
-            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-                super.onFailure(statusCode, headers, responseString, throwable);
-            }
-        });
-        //return items;
-    }//FIN TAREA
 }
